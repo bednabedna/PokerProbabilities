fad889f0c222b8ea5ca8dee0692b3d51d94b9e17
bednabedna <49315422+bednabedna@users.noreply.github.com>
2020-12-29 00:03:30 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -383,7 +383,7 @@ fn random_example() {
 }
 
 #[derive(FromArgs)]
-/// Estimate Poker Texas Holdem winning probability simulating games with the cards provided.
+/// Estimate Poker Texas Holdem winning probabilities simulating games with the cards provided.
 /// Cards are given as a string, for example "4CAQ" means 2 cards: 4 of ♥ and ace of ♦.
 /// The values are '1' or 'A', '2' to '10', 'J' or '11', 'Q' or '12', 'K' or '13'.
 /// Suits are 'C' or '♥', 'Q' or '♦', 'P' or '♠' and 'F' or '♣'.
@@ -406,7 +406,7 @@ struct SimulationArgs {
     games: u32,
 
     #[argh(switch)]
-    /// switch used to request execution time
+    /// display execution time
     time: bool,
 }
 
